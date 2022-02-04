6c824f88c54071bbcacae761ca66f1fb41e88126
sabin <sabin@MOSKAU-NB111>
2022-02-04 11:39:15 +0300

    Add project files.

---
+++ CodeExamplesConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeExamplesConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start a program!");
+            Console.WriteLine();
+
+            //AspNetCoreWebApp.lesson1.example1.Start();
+            //AspNetCoreWebApp.lesson1.example2.Start();
+            AspNetCoreWebApp.lesson1.example3.Start();
+
+            Console.WriteLine();
+            Console.WriteLine("End of program!");
+            Console.Read();
+        }
+    }
+}
